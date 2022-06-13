59b0adbf1e597b729830766e87df440295c19c33
aurelienalbert <aurelienalbert.dev@gmail.com>
2022-06-13 22:14:21 +0200

    Add operations for rooms

--- python/protolk/api.py
+++ python/protolk/api.py
@@ -35,6 +35,45 @@ class ProtolkApiImp:
         """Delete all users."""
         return await self._store.delete_users()
 
+    async def get_room(self, room_id: str):
+        """Get specific room."""
+        return await self._store.get_room(room_id)
+
+    async def get_rooms(self, owner_id: str, user_id: str):
+        """Get all rooms."""
+        return await self._store.get_rooms(
+            owner_id=owner_id,
+            user_id=user_id
+        )
+
+    async def post_room(self, room: Dict):
+        """Add a new room."""
+        if not "id" in room:
+            room["id"] = str(uuid.uuid4())
+        room["users"] = [room["owner_id"]]
+        return await self._store.post_room(room)
+
+    async def delete_room(self, room_id: str):
+        """Delete a specific room."""
+        return await self._store.delete_room(room_id)
+
+    async def delete_rooms(self):
+        """Delete all rooms."""
+        return await self._store.delete_rooms()
+
+    async def put_user_in_room(self, room_id: str, user_id: str):
+        """Add a user in a room."""
+        return await self._store.put_user_in_room(
+            room_id,
+            user_id
+        )
+
+    async def delete_user_from_room(self, room_id: str, user_id: str):
+        """Remove user from a room."""
+        return await self._store.delete_user_from_room(
+            room_id,
+            user_id
+        )
 
 class ProtolkWebApp:
     """Protolk web app"""
@@ -44,7 +83,7 @@ class ProtolkWebApp:
         self.app = web.Application()
 
     def build_users_endpoint(self, routes):
-        """Add users endpoint requests."""
+        """Build users endpoint requests."""
         @routes.get("/users/{user_id}")
         async def get_users(request):
             user = await self.implementation.get_user(request.match_info["user_id"])
@@ -78,9 +117,59 @@ class ProtolkWebApp:
             return web.Response(status=204)
 
     def build_rooms_endpoint(self, routes):
-        pass
+        """Build rooms endpoint requests."""
+        @routes.get("/rooms/{room_id}")
+        async def get_rooms(request):
+            room = await self.implementation.get_room(request.match_info["room_id"])
+            if not room:
+                return web.Response(status=404)
+            return web.json_response(status=200, data=room)
+
+        @routes.get("/rooms")
+        async def get_rooms(request):
+            owner_id = None
+            user_id = None
+            if "ownerId" in request.query:
+                owner_id = request.query["ownerId"]
+            if "userId" in request.query:
+                user_id = request.query["userId"]
+            rooms = await self.implementation.get_rooms(owner_id, user_id)
+            return web.json_response(status=200, data=rooms)
+
+        @routes.post("/rooms")
+        async def post_room(request):
+            body = await request.json()
+            try:
+                await self.implementation.post_room(body)
+                return web.Response(status=201)
+            except pymongo.errors.DuplicateKeyError:
+                return web.Response(status=401)
+
+        @routes.delete("/rooms")
+        async def delete_rooms(request):
+            await self.implementation.delete_rooms()
+            return web.Response(status=204)
+
+        @routes.put("/rooms/{room_id}/users/{user_id}")
+        async def put_user_in_room(request):
+            if await self.implementation.put_user_in_room(
+                request.match_info["room_id"],
+                request.match_info["user_id"]
+            ):
+                return web.Response(status=201)
+            return web.Response(status=202)
+
+        @routes.delete("/rooms/{room_id}/users/{user_id}")
+        async def delete_user_from_room(request):
+            if await self.implementation.delete_user_from_room(
+                request.match_info["room_id"],
+                request.match_info["user_id"]
+            ):
+                return web.Response(status=204)
+            return web.Response(status=202)
 
     def build_sessions_endpoint(self, routes):
+        """Build sessions endpoint requests."""
         pass
 
     def build_app(self):
--- python/protolk/store.py
+++ python/protolk/store.py
@@ -29,14 +29,66 @@ class Store:
     async def post_user(self, user: Dict):
         """Insert a new user."""
         res = await self._users.insert_one(user)
-        return bool(res.inserted_count)
+        return bool(res.acknowledged)
 
-    async def delete_user(self, used_id: str):
+    async def delete_user(self, user_id: str):
         """Delete a specific user."""
-        res = await self._users.delete_one({"id": used_id})
+        res = await self._users.delete_one({"id": user_id})
         return bool(res.deleted_count)
 
     async def delete_users(self):
         """Delete all users."""
         await self._users.delete_many({})
+        return True
+
+    async def get_room(self, room_id: str):
+        """Get a single room."""
+        projection = {"_id": False}
+        return await self._rooms.find_one({"id": room_id}, projection)
+
+    async def get_rooms(self,
+                        owner_id: str=None,
+                        user_id: str=None,
+                        limits: int=10):
+        """Get all rooms."""
+        projection = {"_id": False}
+        mongo_filter = {}
+        if owner_id:
+            mongo_filter["owner_id"] = owner_id
+        if user_id:
+            mongo_filter["users"] = user_id
+        return await self._rooms.find(mongo_filter, projection).to_list(length=limits)
+
+    async def post_room(self, room: Dict):
+        """Insert a new room."""
+        res = await self._rooms.insert_one(room)
+        return bool(res.acknowledged)
+
+    async def put_user_in_room(self, room_id: str, user_id: str):
+        """Add a user in a room."""
+        if await self._rooms.find_one_and_update(
+            filter={"id": room_id, "users": {"$nin": [user_id]}},
+            update={"$push": {"users": user_id}}
+        ):
+            return True
+        return False
+
+    async def delete_user_from_room(self, room_id: str, user_id: str):
+        """Remove user from a room."""
+        res = await self._rooms.find_one_and_update(
+            filter={"id": room_id},
+            update={"$pull": {"users": user_id}}
+        )
+        if user_id in res["users"]:
+            return True
+        return False
+
+    async def delete_room(self, room_id: str):
+        """Delete a specific room."""
+        res = await self._users.delete_one({"id": room_id})
+        return bool(res.deleted_count)
+
+    async def delete_rooms(self):
+        """Delete all rooms."""
+        await self._rooms.delete_many({})
         return True
